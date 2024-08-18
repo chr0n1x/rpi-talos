0708997c08c212954117691508d8564b4128ee24
chr0n1x <heilong24@gmail.com>
2024-08-17 22:17:06 -0400

    fix: iron out some more things; only have to sync twice now

--- k8s/scripts/prepare-argocd.sh
+++ k8s/scripts/prepare-argocd.sh
@@ -7,7 +7,12 @@ kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds?ref=stable
 
 kubectl annotate --overwrite=true -n argocd crd applications.argoproj.io meta.helm.sh/release-name=argocd
 kubectl annotate --overwrite=true -n argocd crd applications.argoproj.io meta.helm.sh/release-namespace=argocd
+kubectl label -n argocd crd applications.argoproj.io app.kubernetes.io/managed-by=Helm
+
 kubectl annotate --overwrite=true -n argocd crd applicationsets.argoproj.io meta.helm.sh/release-name=argocd
 kubectl annotate --overwrite=true -n argocd crd applicationsets.argoproj.io meta.helm.sh/release-namespace=argocd
+kubectl label -n argocd crd applicationsets.argoproj.io app.kubernetes.io/managed-by=Helm
+
 kubectl annotate --overwrite=true -n argocd crd appprojects.argoproj.io meta.helm.sh/release-name=argocd
 kubectl annotate --overwrite=true -n argocd crd appprojects.argoproj.io meta.helm.sh/release-namespace=argocd
+kubectl label -n argocd crd appprojects.argoproj.io app.kubernetes.io/managed-by=Helm
