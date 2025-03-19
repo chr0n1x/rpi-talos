55d1adfbf24025f082d6e80a9c5e82a7231a3899
chr0n1x <heilong24@gmail.com>
2025-03-19 00:37:47 -0400

    fix: nginx tweaks

--- k8s/scripts/ingress-nginx-prep.sh
+++ k8s/scripts/ingress-nginx-prep.sh
@@ -1,9 +1,9 @@
 kubectl create ns ingress-nginx || :
 
-kubectl annotate --overwrite=true ns ingress-nginx meta.helm.sh/release-name=ingress-nginx
-kubectl annotate --overwrite=true ns ingress-nginx meta.helm.sh/release-namespace=ingress-nginx
-kubectl label ns ingress-nginx app.kubernetes.io/managed-by=Helm
+kubectl annotate --overwrite=true ns ingress-nginx meta.helm.sh/release-name=ingress-nginx || :
+kubectl annotate --overwrite=true ns ingress-nginx meta.helm.sh/release-namespace=ingress-nginx || :
+kubectl label ns ingress-nginx app.kubernetes.io/managed-by=Helm || :
 
-kubectl label ns ingress-nginx pod-security.kubernetes.io/audit: privileged
-kubectl label ns ingress-nginx pod-security.kubernetes.io/enforce: privileged
-kubectl label ns ingress-nginx pod-security.kubernetes.io/warn: privileged
+kubectl label ns ingress-nginx "pod-security.kubernetes.io/audit: privileged" || :
+kubectl label ns ingress-nginx "pod-security.kubernetes.io/enforce: privileged" || :
+kubectl label ns ingress-nginx "pod-security.kubernetes.io/warn: privileged" || :
--- k8s/scripts/ingress-nginx-wait.sh
+++ k8s/scripts/ingress-nginx-wait.sh
@@ -2,4 +2,4 @@ kubectl -n ingress-nginx wait --for=condition=available deployment ingress-nginx
 
 kubectl -n ingress-nginx wait job --all --for condition=Complete
 
-kubectl expose deployment ingress-nginx-controller --type=LoadBalancer --name=ingress-nginx-controller
+kubectl expose deployment ingress-nginx-controller --type=LoadBalancer --name=ingress-nginx-controller || :
