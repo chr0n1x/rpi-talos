eda931cc96d7d085a26f3749de0efb1265e78649
Kevin R <heilong24@gmail.com>
2025-11-07 23:44:56 -0500

    feat: etcd defrag timer/cron

--- etc/scripts/etcd-defrag.bash
+++ etc/scripts/etcd-defrag.bash
@@ -11,18 +11,19 @@
 # 2. add the following:
 #
 #   @daily <user> TALOSCONFIG=<path to config> /bin/bash ~/Code/chr0n1x/rpi-talos/etc/scripts/etcd-defrag.bash 
+# 3. or use the systemctl service/timer in ../systemd
 
 NODES=$(kubectl get nodes -o=wide | grep control | awk '{ print $1 }')
-LOG_FILE=/tmp/etcd-defrag.log
+# LOG_FILE=/tmp/etcd-defrag.log
 
 started=false
 info() {
   if [ $started = false ]; then
-    echo -e "[$(date)] $@" > $LOG_FILE
+    echo -e "[$(date)] $@" # > $LOG_FILE
     started=true
     return
   fi
-  echo -e "[$(date)] $@" >> $LOG_FILE
+  echo -e "[$(date)] $@" # >> $LOG_FILE
 }
 
 info "Found control nodes:\n\n$NODES\n"
@@ -34,12 +35,12 @@ info
 info "Starting defrag run..."
 
 talosctl \
-  -n "$(echo $NODES | tr ' ' ',' | sed 's/,$//g')" etcd status >> $LOG_FILE
+  -n "$(echo $NODES | tr ' ' ',' | sed 's/,$//g')" etcd status # >> $LOG_FILE
 
 for cnode in $NODES; do
   info
   info "- defragging control-node $cnode..."
-  talosctl -n $cnode etcd defrag >> $LOG_FILE
+  talosctl -n $cnode etcd defrag # >> $LOG_FILE
 done 
 
 info
