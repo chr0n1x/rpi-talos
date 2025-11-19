3815b7865451627b357caf87c82f791108e59e08
Kevin R <heilong24@gmail.com>
2025-11-19 10:19:47 -0500

    chore: removing ingress-nginx

--- k8s/scripts/ingress-nginx-wait.sh
+++
@@ -1,5 +0,0 @@
-kubectl -n ingress-nginx wait --for=condition=available deployment ingress-nginx-controller
-
-kubectl -n ingress-nginx wait job --all --for condition=Complete
-
-kubectl expose deployment ingress-nginx-controller --type=LoadBalancer --name=ingress-nginx-controller || :
